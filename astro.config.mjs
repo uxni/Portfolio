21d40d3cc0fe2b8b92545e560038b9c94fafd722
Kei <canitesmark2010@gmail.com>
2026-07-15 21:02:27 +0000

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://uxni.github.io/Portfolio',
+  base: '/Portfolio/',
   vite: {
     plugins: [tailwindcss()]
   }
