57d3d5ebc3e129f7a536de009651791b39dc0bc8
Kei <canitesmark2010@gmail.com>
2026-07-16 04:26:54 +0800

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
